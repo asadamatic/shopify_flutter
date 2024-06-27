8a698a8d03b12b7c7dbc2b01d75f1e1f33b5c765
asadamatic <asadamatic@gmail.com>
2024-06-27 17:48:25 +0500

    Create MoneyV2 class.

---
+++ lib/models/src/product/money_v_2/money_v_2.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: invalid_annotation_target
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:intl/intl.dart';
+
+import '../../../json_helper.dart';
+
+part 'money_v_2.freezed.dart';
+part 'money_v_2.g.dart';
+
+@freezed
+class MoneyV2 with _$PriceV2 {
+  static NumberFormat? priceFormat;
+
+  MoneyV2._();
+
+  factory MoneyV2({
+    @JsonKey(fromJson: JsonHelper.amountFromJson) required double amount,
+    required String currencyCode,
+  }) = _PriceV2;
+
+  factory MoneyV2.fromJson(Map<String, dynamic> json) =>
+      _$PriceV2FromJson(json);
+
+  String get formattedPrice => JsonHelper.chooseRightOrderOnCurrencySymbol(
+        amount,
+        currencyCode,
+        priceFormat: priceFormat,
+      );
+
+  String formattedPriceWithLocale(String? locale) =>
+      JsonHelper.chooseRightOrderOnCurrencySymbol(
+        amount,
+        currencyCode,
+        priceFormat: priceFormat,
+        locale: (locale != null && locale.isEmpty) ? null : locale,
+      );
+}
